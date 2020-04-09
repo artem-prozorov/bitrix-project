153082ed002cf5a471306a218993f4c72a6d30f3
Artem Prozorov <artem.g.prozorov@gmail.com>
2020-04-09 13:03:35 +0300

    Расширен функционал трайта для валидации данных

--- App/Traits/Validatable.php
+++ App/Traits/Validatable.php
@@ -3,37 +3,40 @@
 namespace App\Traits;
 
 use Illuminate\Validation\Validator;
-use Illuminate\Translation\{Translator, ArrayLoader};
+use Illuminate\Translation\{ArrayLoader, Translator};
 
 trait Validatable
 {
-    /**
-     * @var		array	$rules
-     */
-    protected $rules = [];
-
-    protected $messages = [
+    protected $defaultMessages = [
         'required' => 'Не указано поле :attribute',
+        'numeric' => 'Поле :attribute должно содержать числовое значение',
     ];
 
     /**
      * Валидирует переданные данные
      *
      * @access	public
-     * @param	array	$data	
+     * @param	array	$data 	
+     * @param	array	$rules	Default: null
      * @return	void
      */
-    public function validate(array $data)
+    public function validate(array $data, array $rules = null, array $messages = null)
     {
+        $rules = $rules ?? (property_exists($this, 'rules') ? $this->rules : []);
+
+        $messages = $messages ?? (property_exists($this, 'messages') ? $this->messages : $this->defaultMessages);
+
+        $locale = property_exists($this, 'locale') ? $this->locale : 'en_US';
+
         $validator = new Validator(
-            new Translator(new ArrayLoader('en_US', ''), 'en_US'),
+            new Translator(new ArrayLoader($locale, ''), $locale),
             $data,
-            $this->rules,
-            $this->messages
+            $rules,
+            $messages
         );
 
         if ($validator->fails()) {
-            throw new \InvalidArgumentException();
+            throw new \InvalidArgumentException(implode(', ', $validator->errors()->all()));
         }
     }
 }
---
+++ tests/Unit/App/Traits/ValidatableTest.php
@@ -0,0 +1,54 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use App\Traits\Validatable;
+
+class ValidatableTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function validator_uses_external_rules_if_provided()
+    {
+        $tester = new class() {
+            use Validatable;
+        };
+
+        $tester->validate(['id' => 1], ['id' => 'required|integer']);
+
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @test
+     */
+    public function validator_uses_internal_rules_if_provided()
+    {
+        $tester = new class() {
+            use Validatable;
+
+            protected $rules = ['id' => 'required|integer'];
+        };
+
+        $tester->validate(['id' => 1]);
+
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @test
+     */
+    public function validator_uses_external_rules_if_internal_also_provided()
+    {
+        $tester = new class() {
+            use Validatable;
+
+            protected $rules = ['this_rule_must_be_overwritten' => 'required|bool'];
+        };
+
+        $tester->validate(['id' => 2], ['id' => 'required|integer']);
+
+        $this->assertTrue(true);
+    }
+}
